e5a604f1eb97af59642e192b47a7e5b801d912e2
상엽 <ksyy1201@gmail.com>
2020-06-28 13:42:39 +0900

    # binary search

--- src/main/java/com/example/demo/sort/BinarySearch.java
+++ src/main/java/com/example/demo/sort/BinarySearch.java
@@ -1,5 +1,7 @@
 package com.example.demo.sort;
 
+import java.util.stream.IntStream;
+
 // O(logn)
 public class BinarySearch {
 
@@ -8,15 +10,15 @@ public class BinarySearch {
         int end = arr.length - 1;
         int mid;
 
-        while(start < end) {
+        while (start <= end) {
             mid = (start + end) / 2;
 
-            if(key == arr[mid]) {
-                System.out.println(mid);
+            if (key == arr[mid]) {
+                System.out.println(arr[mid]);
                 break;
             }
 
-            if(key < arr[mid]) {
+            if (key < arr[mid]) {
                 end = mid - 1;
             } else {
                 start = mid + 1;
@@ -25,8 +27,6 @@ public class BinarySearch {
     }
 
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,6,7,8,9};
-
-        binarySearch(7, arr);
+        binarySearch(42, IntStream.range(0, 100).toArray());
     }
 }

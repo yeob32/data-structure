eba44898c76433b7c4acb92219ac8fc963d02d24
상엽 <ksyy1201@gmail.com>
2020-07-09 20:47:02 +0900

    # 중복이 없는가

---
+++ src/main/java/com/example/demo/question/stringAndArray/CheckDupStr.java
@@ -0,0 +1,29 @@
+package com.example.demo.question.stringAndArray;
+
+public class CheckDupStr {
+
+    public static void main(String[] args) {
+        String str = "hello world!";
+
+        System.out.println(isUniqueStr(str));
+    }
+
+    private static boolean isUniqueStr(String str) {
+        if(str.length() > 128) { // ascii 는 128개임
+            return false;
+        }
+
+        boolean[] checker = new boolean[128];
+
+        for(int i=0; i<str.length(); i++) {
+            char ch = str.charAt(i);
+            if(checker[ch]) {
+                return false;
+            }
+
+            checker[ch] = true;
+        }
+
+        return true;
+    }
+}

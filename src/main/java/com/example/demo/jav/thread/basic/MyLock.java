ffaf679b67f652fd96f7efbd00b0cf150571ecc4
sykim <sykim@i-on.net>
2020-07-27 17:28:20 +0900

    # 정리

--- src/main/java/com/example/demo/jav/thread/basic/MyLock.java
+++ src/main/java/com/example/demo/jav/thread/basic/MyLock.java
@@ -2,8 +2,12 @@ package com.example.demo.jav.thread.basic;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 
+/**
+ * Atomic 자료형이 modern cpu가 기본적으로 제공하는 synchronized compare & swap 기능을 바로 사용하므로 직접 구현할 때보다 더 빠른 성능을 보장한다.
+ */
 public class MyLock {
 
+    private boolean locked2 = false;
     private final AtomicBoolean locked = new AtomicBoolean(false);
 
     static class TestRunnable implements Runnable {
@@ -17,16 +21,19 @@ public class MyLock {
         @Override
         public void run() {
             System.out.println(Thread.currentThread().getName() + ": " + myLock.locking());
+//            System.out.println(Thread.currentThread().getName() + " -- 2: " + myLock.locking2());
         }
     }
 
-    public boolean locking() {
-        if (locked.get()) return false;
-
-        for (int i = 0; i < 100_000; i++) {
-
+    public synchronized boolean locking2() {
+        if(!locked2) {
+            locked2 = true;
+            return true;
         }
+        return false;
+    }
 
+    public boolean locking() {
         return locked.compareAndSet(false, true);
     }
 

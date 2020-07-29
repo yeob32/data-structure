3b6709ce3827ff67708ae4776ef8ab79ee0d1aa6
sykim <sykim@i-on.net>
2020-07-29 10:44:14 +0900

    # 문자열 뒤집기

--- src/main/java/com/example/demo/question/RevereString.java
+++ src/main/java/com/example/demo/question/RevereString.java
@@ -4,39 +4,33 @@ import java.util.Stack;
 
 public class RevereString {
 
-    private static String reverse(String str) {
-        StringBuilder result = new StringBuilder();
+    private static String reverse1(String str) {
+        String result = "";
         for (int i = str.length() - 1; i >= 0; i--) {
-            result.append(str.charAt(i));
+            result += str.charAt(i);
         }
 
-        return result.toString();
+        return result;
     }
 
     public static void main(String[] args) {
-//        String str = "abcdefghijkllmn";
-//        String reversed = reverse(str);
-//        System.out.println(reversed);
-//
-//        StringBuilder stringBuilder = new StringBuilder(str);
-//        System.out.println(stringBuilder.reverse().toString());
-        String temp = "[{()}]";
+        String str = "abcdefghijkllmn";
+        System.out.println(reverse1(str));
 
+        StringBuilder stringBuilder = new StringBuilder(str);
+        System.out.println(stringBuilder.reverse().toString());
+
+        String str2 = "[{()}]";
         Stack<String> stack = new Stack<>();
-        for (int i = temp.length() - 1; i >= 0; i--) {
-            stack.push(Character.toString(temp.charAt(i)));
+        for (int i = str2.length() - 1; i >= 0; i--) {
+            stack.push(Character.toString(str2.charAt(i)));
         }
-
-//        for (int i = 0; i < temp.length(); i++) {
-//            stack.push(Character.toString(temp.charAt(i)));
-//            System.out.println(temp.charAt(i));
-//        }
 
         StringBuilder result = new StringBuilder();
         while (stack.iterator().hasNext()) {
             result.append(stack.pop());
         }
 
-        System.out.println(result.toString().equals(temp));
+        System.out.println(result.toString().equals(str2));
     }
 }

73aea9d315f62db1923382fac9705035d887fcdb
yeob32 <yeob32@gmail.com>
2020-08-11 17:26:06 +0900

    # update case

--- src/main/java/com/example/demo/dpattern/observer/Client.java
+++ src/main/java/com/example/demo/dpattern/observer/Client.java
@@ -4,9 +4,15 @@ public class Client {
 
     public static void main(String[] args) {
         User user = new User();
+        User user2 = new User();
 
         BookStore bookStore = new BookStore();
         bookStore.subscribe(user);
+        bookStore.subscribe(user2);
+
+        bookStore.release();
+
+        bookStore.unsubscribe(user2);
 
         bookStore.release();
     }
